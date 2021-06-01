0c88f0b27ae442851a0385ced842d54b197b80fc
CamilleAbella <camille.abella@hotmail.fr>
2021-06-01 22:26:09 +0200

    add guildMemberRemove listener

---
+++ src/listeners/guildMemberRemove.ts
@@ -0,0 +1,26 @@
+import * as app from "../app"
+
+const listener: app.Listener<"guildMemberRemove"> = {
+  event: "guildMemberRemove",
+  async run(member) {
+    app
+      .getCategory(member, "memberCount")
+      .setName(`STRADIVARIUS | ${member.guild.memberCount} MEMBRES`)
+      .catch()
+    app
+      .getChannel(member, "log")
+      .send(
+        new app.MessageEmbed()
+          .setTitle("Ancien membre")
+          .setDescription(
+            `**${
+              member.user?.tag ?? member.displayName
+            }** vient de quitter le serveur.`
+          )
+          .setColor(app.ALERT)
+      )
+      .catch()
+  },
+}
+
+module.exports = listener
