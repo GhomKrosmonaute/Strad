c02cbfb73f6519edb1733ec55ec54fb668492fc7
CamilleAbella <camille.abella@hotmail.fr>
2021-06-01 19:00:22 +0200

    add repo command

---
+++ src/commands/repo.ts
@@ -0,0 +1,27 @@
+import * as app from "../app"
+
+module.exports = new app.Command({
+  name: "repo",
+  description: "The repo command",
+  channelType: "guild",
+  async run(message) {
+    const repos = {
+      Nouveau: "https://github.com/CamilleAbella/Strad",
+      Ancien: "https://github.com/Tagueo/new-strad",
+    }
+    return app.getCommandChannel(message).send(
+      new app.MessageEmbed()
+        .setAuthor("Dépôts de Strad", message.client.user.displayAvatarURL())
+        .setColor("#21b1ff")
+        .setDescription(
+          `Tu veux participer à mon développement ? Tu peux contribuer dès maintenant sur mon dépôt GitHub :smile:\nN'oublie pas de mettre une petite étoile si tu me trouves cool :blue_heart:`
+        )
+        .addField(
+          "Liens",
+          Object.entries(repos)
+            .map(([label, link]) => `${label}: ${link}`)
+            .join("\n")
+        )
+    )
+  },
+})
