8611333b7d485349668d453468e6132adb7598be
CamilleAbella <camille.abella@hotmail.fr>
2021-12-19 18:05:22 +0100

    update bot.ts

--- src/listeners/guildBanAdd.ts
+++ src/listeners/guildBanAdd.ts
@@ -4,16 +4,14 @@ const listener: app.Listener<"guildBanAdd"> = {
   event: "guildBanAdd",
   description: "Ban message",
   async run(ban) {
-    return app
-      .getChannel(ban, "log")
-      .send({
-        embeds: [
-          new app.MessageEmbed()
-            .setTitle("Membre Banni")
-            .setDescription(`**${ban.user.tag}** vient de se faire bannir.`)
-            .setColor("#f44242"),
-        ],
-      })
+    return app.getChannel(ban, "log").send({
+      embeds: [
+        new app.MessageEmbed()
+          .setTitle("Membre Banni")
+          .setDescription(`**${ban.user.tag}** vient de se faire bannir.`)
+          .setColor("#f44242"),
+      ],
+    })
   },
 }
 
--- src/listeners/ready.strad.ts
+++ src/listeners/ready.strad.ts
@@ -5,11 +5,10 @@ const listener: app.Listener<"ready"> = {
   description: "Set rich presence",
   once: true,
   async run() {
-    this.user
-      .setActivity(`Strad help`, {
-        type: "STREAMING",
-        url: "https://www.twitch.tv/delphistudio",
-      })
+    this.user.setActivity(`Strad help`, {
+      type: "STREAMING",
+      url: "https://www.twitch.tv/delphistudio",
+    })
   },
 }
 
